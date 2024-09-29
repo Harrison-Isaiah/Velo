4e0ff7ddde82f8de8b1dce9228fc0485bc702eab
Harrison Isaiah <97739869+Harrison-Isaiah@users.noreply.github.com>
2024-09-29 04:13:47 -0700

    Create section3.js

---
+++ src/section3.js
@@ -0,0 +1,86 @@
+import React from 'react';
+
+function renderSection3(props) {
+  return (
+    <section className="w-[1920px] h-[955px] relative min-h-[955px]">
+      /*🚩21 Unstructured items require structuring to ensure responsiveness, as code with absolute positioning lacks
+      adaptability.❗*/
+      <h1 className="flex justify-center w-[841px] h-[73px] absolute left-[540px] right-[539px] top-[200px] bottom-[682px]">
+        <span className="font-semibold text-[60px] leading-[0.7] font-Montserrat text-black text-center uppercase">
+          <span className="font-semibold leading-[1.21] text-[rgb(246,255,255)]">selected </span>
+          <span className="font-semibold leading-[1.21] text-[rgb(74,222,222)]">design works</span>
+        </span>
+      </h1>
+      <hr
+        className="bg-[image:linear-gradient(90deg,_rgba(226,_235,_235,_0.00)_0%,_rgba(246,_255,_255,_0.49)_51.5%,_rgba(226,_235,_235,_0.00)_100%)] w-[510px] h-px absolute left-0 right-[1410px] top-[216px] bottom-[738px]"
+        size={1}
+      />
+      <hr
+        className="bg-[image:linear-gradient(270deg,_rgba(226,_235,_235,_0.00)_0%,_rgba(246,_255,_255,_0.49)_51.5%,_rgba(226,_235,_235,_0.00)_100%)] w-[509px] h-px absolute left-[1411px] right-0 top-[217px] bottom-[737px]"
+        size={1}
+      />
+      <hr
+        className="bg-[image:linear-gradient(180deg,_rgba(74,_222,_222,_0.00)_0%,_#287878_263.33%)] w-[1920px] h-px absolute left-0 right-0 top-[468px] bottom-[486px]"
+        size={1}
+      />
+      <hr
+        className="bg-[image:linear-gradient(90deg,_rgba(226,_235,_235,_0.00)_0%,_rgba(246,_255,_255,_0.49)_51.5%,_rgba(226,_235,_235,_0.00)_100%)] w-[1920px] h-px absolute left-0 right-0 top-[468px] bottom-[486px]"
+        size={1}
+      />
+      <h1 className="font-normal text-[40px] leading-[0.7] font-Montserrat text-[rgb(246,255,255)] uppercase w-[385px] h-7 absolute left-[90px] right-[1445px] top-[381px] bottom-[546px]">
+        Versatile Group
+      </h1>
+      <p className="font-normal text-[18px] leading-[0.72] font-Montserrat text-[rgb(246,255,255)] uppercase w-[163px] h-[13px] absolute left-[1130px] right-[627px] top-[388px] bottom-[554px]">
+        Graphic design
+      </p>
+      <p className="font-normal text-[18px] leading-[0.72] font-Montserrat text-[rgb(246,255,255)] uppercase w-36 h-[13px] absolute left-[1656px] right-[120px] top-[388px] bottom-[554px]">
+        development
+      </p>
+      <p className="font-normal text-[18px] leading-[0.72] font-Montserrat text-[rgb(246,255,255)] uppercase w-[123px] h-[13px] absolute left-[1413px] right-96 top-[388px] bottom-[554px]">
+        web design
+      </p>
+      <hr
+        className="bg-[image:linear-gradient(180deg,_rgba(74,_222,_222,_0.00)_0%,_#287878_263.33%)] w-[1920px] h-px absolute left-0 right-0 top-[648px] bottom-[306px]"
+        size={1}
+      />
+      <hr
+        className="bg-[image:linear-gradient(90deg,_rgba(226,_235,_235,_0.00)_0%,_rgba(246,_255,_255,_0.49)_51.5%,_rgba(226,_235,_235,_0.00)_100%)] w-[1920px] h-px absolute left-0 right-0 top-[648px] bottom-[306px]"
+        size={1}
+      />
+      <h1 className="font-normal text-[40px] leading-[0.7] font-Montserrat text-[rgb(246,255,255)] uppercase w-[560px] h-7 absolute left-[90px] right-[1270px] top-[561px] bottom-[366px]">
+        Versatile bim solutions
+      </h1>
+      <p className="font-normal text-[18px] leading-[0.72] font-Montserrat text-[rgb(246,255,255)] uppercase w-[163px] h-[13px] absolute left-[1130px] right-[627px] top-[568px] bottom-[374px]">
+        Graphic design
+      </p>
+      <p className="font-normal text-[18px] leading-[0.72] font-Montserrat text-[rgb(246,255,255)] uppercase w-36 h-[13px] absolute left-[1656px] right-[120px] top-[568px] bottom-[374px]">
+        development
+      </p>
+      <p className="font-normal text-[18px] leading-[0.72] font-Montserrat text-[rgb(246,255,255)] uppercase w-[123px] h-[13px] absolute left-[1413px] right-96 top-[568px] bottom-[374px]">
+        web design
+      </p>
+      <hr
+        className="bg-[image:linear-gradient(180deg,_rgba(74,_222,_222,_0.00)_0%,_#287878_263.33%)] w-[1920px] h-px absolute left-0 right-0 top-[828px] bottom-[126px]"
+        size={1}
+      />
+      <hr
+        className="bg-[image:linear-gradient(90deg,_rgba(226,_235,_235,_0.00)_0%,_rgba(246,_255,_255,_0.49)_51.5%,_rgba(226,_235,_235,_0.00)_100%)] w-[1920px] h-px absolute left-0 right-0 top-[828px] bottom-[126px]"
+        size={1}
+      />
+      <h1 className="font-normal text-[40px] leading-[0.7] font-Montserrat text-[rgb(246,255,255)] uppercase w-[426px] h-7 absolute left-[90px] right-[1404px] top-[741px] bottom-[186px]">
+        Versatile shading
+      </h1>
+      <p className="font-normal text-[18px] leading-[0.72] font-Montserrat text-[rgb(246,255,255)] uppercase w-[163px] h-[13px] absolute left-[1130px] right-[627px] top-[748px] bottom-[194px]">
+        Graphic design
+      </p>
+      <p className="font-normal text-[18px] leading-[0.72] font-Montserrat text-[rgb(246,255,255)] uppercase w-36 h-[13px] absolute left-[1656px] right-[120px] top-[748px] bottom-[194px]">
+        development
+      </p>
+      <p className="font-normal text-[18px] leading-[0.72] font-Montserrat text-[rgb(246,255,255)] uppercase w-[123px] h-[13px] absolute left-[1413px] right-96 top-[748px] bottom-[194px]">
+        web design
+      </p>
+    </section>
+  );
+}
+
+export default renderSection3;
