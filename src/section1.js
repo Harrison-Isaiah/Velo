307278fabd990644fe574f6e457bd251602e7765
Harrison Isaiah <97739869+Harrison-Isaiah@users.noreply.github.com>
2024-09-29 04:12:30 -0700

    Create section1.js

---
+++ src/section1.js
@@ -0,0 +1,134 @@
+import React from 'react';
+
+function renderSection1(props) {
+  return (
+    <section className="w-[1920px] h-[3640px] relative min-h-[3640px]">
+      /*🚩32 Unstructured items require structuring to ensure responsiveness, as code with absolute positioning lacks
+      adaptability.❗*/
+      <img
+        className="w-[1920px] h-[1958px] absolute left-0 right-0 top-[1080px] bottom-[602px]"
+        src={'/assets/ae3244995428efbc2517797dcf421ded.png'}
+        alt="alt text"
+      />
+      <img
+        className="w-[1920px] h-[1258px] absolute left-0 right-0 top-0 bottom-[2382px]"
+        src={'/assets/9f89e56d60ba52da615f2d568a9b8a3d.png'}
+        alt="alt text"
+      />
+      <img
+        className="w-[580px] h-[501px] absolute left-[670px] right-[670px] top-[658px] bottom-[2481px]"
+        src={'/assets/d11b1e37ac9e4b6cdc78bcf40be6af72.png'}
+        alt="alt text"
+      />
+      <h1 className="font-normal text-[128px] leading-[0.7] font-Montserrat text-[rgb(246,255,255)] uppercase w-[749px] h-[90px] absolute left-[586px] right-[585px] top-[490px] bottom-[3060px]">
+        Creativity
+      </h1>
+      <h1 className="font-normal text-[36px] leading-[0.69] font-Montserrat text-[rgb(246,255,255)] uppercase w-[426px] h-[25px] absolute left-[747px] right-[747px] top-[620px] bottom-[2995px]">
+        Elevate Your Brand
+      </h1>
+      <h1 className="font-normal text-[36px] leading-[0.69] font-Montserrat text-[rgb(246,255,255)] uppercase tracking-[25.56px] w-[360px] h-[25px] absolute left-[793px] right-[767px] top-[435px] bottom-[3180px]">
+        UNLEASH
+      </h1>
+      <div className="border-solid border-[rgb(246,255,255)] border-t-[0px] border-r-[0px] border-b-[1px] border-l-[0px] w-[1920px] h-[150px] absolute left-0 right-0 top-0 bottom-[3490px]" />
+      <img
+        className="w-[232px] h-[51px] absolute left-[90px] right-[1598px] top-[50px] bottom-[3539px]"
+        src={'/assets/21fe782cc25f47af3725f86598a53687.svg'}
+        alt="alt text"
+      />
+      <h3 className="font-normal text-[20px] leading-[0.7] font-Montserrat text-white uppercase w-[164px] h-3.5 absolute left-[505px] right-[1251px] top-[68px] bottom-[3558px]">
+        our expertise
+      </h3>
+      <h3 className="font-normal text-[20px] leading-[0.7] font-Montserrat text-white uppercase w-[151px] h-3.5 absolute left-[789px] right-[980px] top-[68px] bottom-[3558px]">
+        our process
+      </h3>
+      <h3 className="font-normal text-[20px] leading-[0.7] font-Montserrat text-white uppercase w-[165px] h-3.5 absolute left-[1060px] right-[695px] top-[68px] bottom-[3558px]">
+        Design works
+      </h3>
+      <h3 className="font-normal text-[20px] leading-[0.7] font-Montserrat text-white uppercase w-[69px] h-3.5 absolute left-[1345px] right-[506px] top-[68px] bottom-[3558px]">
+        tools
+      </h3>
+      <div className="bg-[image:radial-gradient(107.5%_107.5%_at_50%_215%,_rgba(255,_255,_255,_0.08)_0%,_rgba(255,_255,_255,_0.00)_100%)] bg-[rgba(255,255,255,0.007)] rounded-[22px] outline outline-[rgba(255,255,255,0.098)] outline-1 outline-offset-[-1px] w-48 h-11 absolute left-[1638px] right-[90px] top-[53px] bottom-[3543px]" />
+      <h3 className="font-normal text-[20px] leading-[0.7] font-Montserrat text-[rgb(246,255,255)] w-[132px] h-3.5 absolute left-[1668px] right-[120px] top-[68px] bottom-[3558px]">
+        CONTACT US
+      </h3>
+      <img
+        className="rounded-[22px] outline outline-white outline-1 outline-offset-[-1px] w-48 h-11 absolute left-[1638px] right-[90px] top-[53px] bottom-[3543px]"
+        src={'/assets/5b12731bf146c0ad708bc99ae78eaaa3.png'}
+        alt="alt text"
+      />
+      <div
+        className="bg-[image:var(--src)] bg-[center_center] bg-[length:100%_100%] bg-no-repeat w-[1920px] h-[1080px] overflow-hidden absolute left-0 right-0 top-[1280px] bottom-[1280px] min-h-[1080px]"
+        style={{ '--src': `url(${'/assets/c941f9d04d8f60020840a96c692ac3c0.png'})` }}>
+        /*🚩8 Unstructured items require structuring to ensure responsiveness, as code with absolute positioning lacks
+        adaptability.❗*/
+        <div className="bg-[image:linear-gradient(0deg,_#060606_0%,_rgba(6,_6,_6,_0.00)_100%)] w-[1920px] h-24 absolute left-0 right-0 top-[984px] bottom-0" />
+        <h1 className="font-normal text-[64px] leading-[0.7] font-Montserrat text-[rgb(246,255,255)] uppercase w-[523px] h-[45px] absolute left-[699px] right-[698px] top-[60px] bottom-[975px]">
+          OUR EXPERTISE
+        </h1>
+        <h1 className="font-normal text-[32px] leading-[0.68] font-Montserrat text-[rgb(246,255,255)] uppercase tracking-[20.61px] w-[444px] h-[22px] absolute left-[738px] right-[738px] top-2 bottom-[1050px]">
+          YOUR VISION
+        </h1>
+        <h3 className="flex justify-center font-normal text-[20px] leading-[0.7] font-Montserrat text-[rgb(74,222,222)] text-center capitalize w-[123px] h-3.5 absolute left-[626px] right-[1171px] top-[165px] bottom-[901px]">
+          Web Design
+        </h3>
+        <h3 className="flex justify-center font-normal text-[20px] leading-[0.7] font-Montserrat text-[rgb(246,255,255)] text-center capitalize w-[155px] h-3.5 absolute left-[1167px] right-[598px] top-[165px] bottom-[901px]">
+          Graphic design
+        </h3>
+        <h3 className="flex justify-center font-normal text-[20px] leading-[0.7] font-Montserrat text-[rgb(246,255,255)] text-center capitalize w-[118px] h-3.5 absolute left-[901px] right-[901px] top-[165px] bottom-[901px]">
+          app Design
+        </h3>
+        <div className="bg-[image:linear-gradient(90deg,_rgba(226,_235,_235,_0.00)_0%,_rgba(246,_255,_255,_0.49)_51.5%,_rgba(226,_235,_235,_0.00)_100%)] w-[1920px] h-px absolute left-0 right-0 top-52 bottom-[871px]" />
+        <div className="bg-[rgb(74,222,222)] w-[143px] h-[3px] absolute left-[616px] right-[1161px] top-[206px] bottom-[871px]" />
+      </div>
+      <img
+        className="w-[1920px] h-[1080px] absolute left-0 right-0 top-[2560px] bottom-0"
+        src={'/assets/5a7019f26a3d074306d436e192eecab9.png'}
+        alt="alt text"
+      />
+      <img
+        className="w-[1920px] h-24 absolute left-0 right-0 top-[3544px] bottom-0"
+        src={'/assets/f0a1310927ca5876d17a7b55acff8233.png'}
+        alt="alt text"
+      />
+      <img
+        className="w-[833px] h-[1080px] absolute left-[1087px] right-0 top-[2560px] bottom-0"
+        src={'/assets/72e684dc398b594608a365f7c74630ef.png'}
+        alt="alt text"
+      />
+      <img
+        className="w-[1920px] h-[171px] absolute left-0 right-0 top-[2560px] bottom-[909px]"
+        src={'/assets/fea591a99c43a8875b21e5594fb42539.png'}
+        alt="alt text"
+      />
+      <div className="bg-[image:linear-gradient(0deg,_#060606_0%,_rgba(6,_6,_6,_0.00)_100%)] w-[1920px] h-[187px] absolute left-0 right-0 top-[3453px] bottom-0" />
+      <h1 className="w-[499px] h-[73px] absolute left-[87px] right-[1334px] top-[3032px] bottom-[535px]">
+        <span className="font-normal text-[60px] leading-[0.7] font-Montserrat text-black uppercase">
+          <span className="leading-[1.21] text-[rgb(246,255,255)]">Feel</span>
+          <span className="leading-[1.21] text-[rgb(74,222,222)]"> Virtually</span>
+        </span>
+      </h1>
+      <h3 className="font-light text-[20px] leading-[1.43] font-Montserrat text-[rgb(246,255,255)] capitalize w-[851px] h-[58px] absolute left-[90px] right-[979px] top-[3124px] bottom-[458px]">{`Lorem Ipsum is simply dummy text of the printing and typesetting industry  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,`}</h3>
+      <h1 className="font-light text-[30px] leading-[0.7] font-Montserrat text-[rgb(246,255,255)] uppercase tracking-[19.09px] w-[386px] h-[21px] absolute left-[90px] right-[1444px] top-[2981px] bottom-[638px]">
+        Design You
+      </h1>
+      <p className="flex justify-center font-normal text-[18px] leading-[0.72] font-Montserrat text-[rgb(246,255,255)] text-center capitalize w-[62px] h-[13px] absolute left-[107px] right-[1751px] top-[3207px] bottom-[420px]">
+        inspire
+      </p>
+      <div className="bg-[rgb(74,222,222)] rounded w-[7px] h-[7px] absolute left-[90px] right-[1823px] top-[3210px] bottom-[423px]" />
+      <p className="flex justify-center font-normal text-[18px] leading-[0.72] font-Montserrat text-[rgb(246,255,255)] text-center capitalize w-[59px] h-[13px] absolute left-[216px] right-[1645px] top-[3207px] bottom-[420px]">
+        Create
+      </p>
+      <div className="bg-[rgb(74,222,222)] rounded w-[7px] h-[7px] absolute left-[199px] right-[1714px] top-[3210px] bottom-[423px]" />
+      <p className="flex justify-center font-normal text-[18px] leading-[0.72] font-Montserrat text-[rgb(246,255,255)] text-center capitalize w-[71px] h-[13px] absolute left-[322px] right-[1527px] top-[3207px] bottom-[420px]">
+        Engage
+      </p>
+      <div className="bg-[rgb(74,222,222)] rounded w-[7px] h-[7px] absolute left-[305px] right-[1608px] top-[3210px] bottom-[423px]" />
+      <p className="flex justify-center font-normal text-[18px] leading-[0.72] font-Montserrat text-[rgb(246,255,255)] text-center capitalize w-[92px] h-[13px] absolute left-[440px] right-[1388px] top-[3207px] bottom-[420px]">
+        Transform
+      </p>
+      <div className="bg-[rgb(74,222,222)] rounded w-[7px] h-[7px] absolute left-[423px] right-[1490px] top-[3210px] bottom-[423px]" />
+    </section>
+  );
+}
+
+export default renderSection1;
